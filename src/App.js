ff3a294408b7b7cb668604b122a65f3d323787bb
JDumadag25 <justin.dumadag@gmail.com>
2019-07-19 22:40:40 -0400

    revert to 7799151b4cb2f726da9e8e0f82361e9ce8c5b594

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch, withRouter } from 'react-router-dom';
 
 import LogIn from './components/LogIn'
 import Register from './components/Register'
-import Homepage from './components/Homepage'
 
 class App extends React.Component{
 
-
   render(){
     return(
-      <div>
-
-      <LogIn/>
-
-      </div>
+      <Register/>
     )
   }
 }
